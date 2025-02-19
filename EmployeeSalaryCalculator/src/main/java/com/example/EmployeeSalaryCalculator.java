e16d3f0c205cad8eb95baadcbba781144e52c51d
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-02-19 17:48:36 -0500

    final commit

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -56,21 +56,27 @@ public class EmployeeSalaryCalculator
 		*/
 
 
-				if(HW <= standardWorkingHours)
-				{
-					//YOUR CODE STARTS HERE
-					 
+		if(HW <= standardWorkingHours)
+		{
+			//YOUR CODE STARTS HERE
+			basicPay = HW * HR;
+			overTime = 0;
+			overTimeRate = 0;
+			overTimePay = 0;
+			//YOUR CODE ENDS HERE
+		}
+		else
+		{
+			//YOUR CODE STARTS HERE
+			basicPay = standardWorkingHours * HR;
+			overTime = HW - standardWorkingHours;
+			overTimeRate = HR * 2;
+			overTimePay = overTime * overTimeRate;
+			//YOUR CODE ENDS HERE
+		}
 
-					//YOUR CODE ENDS HERE
-				}
-				else
-				{
-					//YOUR CODE STARTS HERE
- 
-
-					//YOUR CODE ENDS HERE
-				}
-				return grossSalary;
+		grossSalary = basicPay + overTimePay;
+		return grossSalary;
 	}
 	
 	static void displayPaySlip()
